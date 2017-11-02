f2cecf95dfc239eb01a40e95df96058412d0ad63
Neel Patel <neelpatel6294@gmail.com>
2017-11-02 09:38:58 -0400

    1

--- app/src/main/java/com/example/patel/gooogleeprod/Phones.java
+++ app/src/main/java/com/example/patel/gooogleeprod/Phones.java
@@ -2,6 +2,7 @@ package com.example.patel.gooogleeprod;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class Phones extends AppCompatActivity {
 
@@ -9,5 +10,15 @@ public class Phones extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_phones);
+
+        String[] goophones = new String[10];
+        goophones[1] = "Google Pixel XL";
+        goophones[2] = "Google Pixel ";
+        goophones[3] = "Google 6P";
+        goophones[4] = "Google 6";
+
+        Log.v("Phones","Word at 1: " +goophones[1]);
+        Log.v("Phones","Word at 1: " +goophones[3]);
+
     }
 }
